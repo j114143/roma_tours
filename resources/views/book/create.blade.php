283b6016f95b8a830fd01960d68a78d0fed10906
j114143 <juan@llika.net>
2016-02-03 12:12:17 -0500

    formulario de reservar tour

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -12,32 +12,28 @@ Reservar
     <p><b>Bus</b>: {{$obj->bus->placa}}</p>
     <p><b>Número asientos</b>: {{$obj->bus->cantidad_asientos}}</p>
     <p><b>Tipo de Bus</b>: {{$obj->bus->tipo->nombre}}</p>
-
+</div>
+<div class="col-sm-4">
     <div class="form-group">
     <input type="text" name="user" placeholder="Lugar de inicio" class="form-control">
     </div>
     <div class="form-group">
     <input type="text" name="user" placeholder="Lugar de finalización" class="form-control">
     </div>
-</div>
-<div class="col-sm-4">
-    <h2 class="text-center">Registrar</h2>
+    <h2 class="text-center">Sus datos</h2>
     <form>
+
         <div class="form-group">
-        <input type="text" name="user" placeholder="Nombre" class="form-control">
+            <input type="text" name="user" placeholder="Nombre o Razon Social" class="form-control">
         </div>
         <div class="form-group">
-        <input type="text" name="user" placeholder="Apellidos" class="form-control">
+            <input type="text" name="user" placeholder="DNI ó RUC" class="form-control">
         </div>
         <div class="form-group">
-        <input type="text" name="user" placeholder="Teléfono" class="form-control">
-        </div>
-        <hr>
-        <div class="form-group">
-        <input type="email" name="user" placeholder="E-mail" class="form-control">
+            <input type="text" name="user" placeholder="Teléfono" class="form-control">
         </div>
         <div class="form-group">
-        <input type="password" name="pass" placeholder="Password"class="form-control">
+            <input type="email" name="user" placeholder="E-mail" class="form-control">
         </div>
         <p class="text-right"><input type="submit" name="login" value="Registrarse" class="btn btn-info"></p>
       </form>
